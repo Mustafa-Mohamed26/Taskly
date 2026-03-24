2ecddeeae532afdf649c8fb3c6dba94dc04ac05e
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2026-03-24 11:33:31 +0200

    Remove theme, todo, and note detail providers along with associated UI components and widgets to streamline the application structure. This includes the deletion of theme management, todo management, and all related screens and widgets, ensuring a cleaner codebase and improved maintainability.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list_app/providers/todo_provider.dart';
-import 'package:todo_list_app/providers/theme_provider.dart';
-import 'package:todo_list_app/screens/home_screen.dart';
 
 void main() {
-  runApp(
-    // providers
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => TodoProvider()),
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,17 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Todo List App',
-      // set up for the Dark mode
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: themeProvider.themeMode,
-
-      home: HomeScreen(),
+      // set up for the Dark mode   
     );
   }
 }
